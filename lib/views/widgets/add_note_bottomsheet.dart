9640f6a5856ce460479198bbbbea01d816b5ac2b
moustafa-hesham <113013570+moustafa-hesham@users.noreply.github.com>
2023-05-28 23:11:58 +0300

    create Custo text field

--- lib/views/widgets/add_note_bottomsheet.dart
+++ lib/views/widgets/add_note_bottomsheet.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:quick_notes/views/widgets/custom_textfield.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 24,
+          ),
+          CustomTextField()
+        ],
+      ),
+    );
   }
 }
---
+++ lib/views/widgets/custom_textfield.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:quick_notes/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: 'Title',
+        hintStyle: TextStyle(color: Colors.white),
+        border: buildboarder(),
+        enabledBorder: buildboarder(),
+        focusedBorder: buildboarder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildboarder([color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(
+          color: color ?? Colors.white,
+        ));
+  }
+}
