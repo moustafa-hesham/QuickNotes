34fb82bd3a6a05afd6cf17b4c9ee2d25f8a2b50d
moustafa-hesham <113013570+moustafa-hesham@users.noreply.github.com>
2023-05-28 23:35:15 +0300

    added custom button

--- lib/views/widgets/add_note_bottomsheet.dart
+++ lib/views/widgets/add_note_bottomsheet.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quick_notes/constants.dart';
 import 'package:quick_notes/views/widgets/custom_textfield.dart';
+
+import 'custom_buttom.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
@@ -8,15 +11,24 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 24,
-          ),
-          CustomTextField(hint: 'title'),
-          SizedBox(height: 16),
-          CustomTextField(hint: 'content', maxLines: 5),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 24,
+            ),
+            CustomTextField(hint: 'title'),
+            SizedBox(height: 16),
+            CustomTextField(hint: 'content', maxLines: 5),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButtom(),
+            SizedBox(
+              height: 40,
+            )
+          ],
+        ),
       ),
     );
   }
