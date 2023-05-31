69f66c952eaf31a874abeaf368b822ab365d3c06
moustafa-hesham <113013570+moustafa-hesham@users.noreply.github.com>
2023-05-31 19:07:00 +0300

    take input and validate data

--- lib/views/widgets/custom_textfield.dart
+++ lib/views/widgets/custom_textfield.dart
@@ -4,12 +4,22 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:quick_notes/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
+  const CustomTextField(
+      {super.key, required this.hint, this.maxLines = 1, this.onSaved});
   final String hint;
   final int maxLines;
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return 'Field is required';
+        } else {
+          return null;
+        }
+      },
       maxLines: maxLines,
       cursorColor: kPrimaryColor,
       decoration: InputDecoration(
