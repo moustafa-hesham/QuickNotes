197a30418a152c31cd1d8cc422c39193e192debe
moustafa-hesham <113013570+moustafa-hesham@users.noreply.github.com>
2023-05-31 21:09:39 +0300

    provide addnote cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:quick_notes/cubits/Add_note_cubit/add_note_cubit.dart';
 import 'package:quick_notes/views/notes.view.dart';
 
 import 'constants.dart';
@@ -15,13 +17,18 @@ class QuickNotes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+        ),
+        home: const NotesView(),
       ),
-      home: const NotesView(),
     );
   }
 }
